0331c16444c6fe7f04ab165ad79c4c0f915afd73
mdenero <mdnero.td@gmail.com>
2024-12-11 20:05:32 -0300

    Nova alteração

--- src/Teste.java
+++ src/Teste.java
@@ -1,7 +1,19 @@
+import java.text.DecimalFormat;
+
 public class Teste {
 public static void main(String[] args) {
+    	double a = 5.0;
+        double b = 7.1;
+
+        DecimalFormat df = new DecimalFormat("#.#####");
+
+        double media = (a+b)/2;
+
+        String result = df.format(media);
+
+
+        System.err.println("Media: " + result);
     
-    System.out.println("TESTANDO GITHUB");
 }
 
         
